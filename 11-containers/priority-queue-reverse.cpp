cea4e5cda8ec2953a495771506399e21811ac5be
Ewout Merckx <ewmerckx.merckx@ugent.be>
2015-08-26 21:46:57 +0200

    priority queue reverse added

---
+++ 11-containers/priority-queue-reverse.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::queue;
+using std::priority_queue;
+using std::pair;
+
+int main() {
+
+    vector <int> v = {5, 6, 7, 9, 1, 2, 4, 2, 6, 1, 2, 9};
+
+    // normal queue ______________________________________________________
+    queue <int> q1;
+    for (int i = 0; i < v.size(); i++) {
+        q1.push(v[i]);
+    }
+    cout << "queue" << endl;
+    while (!q1.empty()) {
+        cout << q1.front() << " ";
+        q1.pop();
+    }
+    cout << endl << endl;
+
+    // priority queue ____________________________________________________
+    priority_queue <int> q2;
+    for (int i = 0; i < v.size(); i++) {
+        q2.push(v[i]);
+    }
+    cout << "priority queue" << endl;
+    while (!q2.empty()) {
+        cout << q2.top() << " ";
+        q2.pop();
+    }
+    cout << endl << endl;
+
+    // priority queue reversed ___________________________________________
+    priority_queue <int, vector<int>, std::greater<int>> q3;
+    for (int i = 0; i < v.size(); i++) {
+        q3.push(v[i]);
+    }
+    cout << "priority queue" << endl;
+    while (!q3.empty()) {
+        cout << q3.top() << " ";
+        q3.pop();
+    }
+    cout << endl << endl;
+
+    // priority queue reversed with pairs ________________________________
+    vector <char> c = {'a', 'k', 'p', 'n', 'v', 'v', 'm', 'd', 'a', 'w', 'y', 'b'};
+    priority_queue < pair < int, char >, vector < pair < int, char >>, std::greater < pair < int, char >> > q4;
+    for (int i = 0; i < v.size(); i++) {
+        q4.push(pair<int, char>(v[i], c[i]));
+    }
+    cout << "priority queue" << endl;
+    while (!q4.empty()) {
+        cout << q4.top().first << "," << q4.top().second << " ";
+        q4.pop();
+    }
+    cout << endl << endl;
+
+
+    return 0;
+}
