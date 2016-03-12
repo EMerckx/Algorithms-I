675daaadd21c1382672427cdd4a5078099a9f2d2
Ewout Merckx <ewoutmerckx.pro@gmail.com>
2016-03-12 16:31:03 +0100

    move operators added to insertion sort

--- 01-insertionsort/insertionsort.cpp
+++ 01-insertionsort/insertionsort.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 
 using std::cout;
 using std::ostream;
 using std::endl;
 using std::vector;
+using std::move;
 
 template<typename T>
 void insertionsort(vector <T> &v);
@@ -28,13 +30,13 @@ int main() {
 template<typename T>
 void insertionsort(vector <T> &v) {
     for (int i = 1; i < v.size(); i++) {
-        T temp = v[i];
+        T temp = move(v[i]);
         int j = i - 1;
         while (j >= 0 && temp < v[j]) {
-            v[j + 1] = v[j];
+            v[j + 1] = move(v[j]);
             j--;
         }
-        v[j + 1] = temp;
+        v[j + 1] = move(temp);
     }
 }
 
