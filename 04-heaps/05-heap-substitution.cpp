2d4abbb9bbfb9ef2223f6141284b25a04f31409c
Ewout Merckx <ewoutmerckx.pro@gmail.com>
2016-03-14 14:16:57 +0100

    added comments

--- 04-heaps/05-heap-substitution.cpp
+++ 04-heaps/05-heap-substitution.cpp
@@ -76,7 +76,10 @@ void substitute_in_heap(vector <T> &v, int index, T& element){
         // insert the element at the specified index
         v[index] = move(element);
 
-        //section: parent is smaller------------------------------
+        // section: parent is smaller-----------------------------
+        // first part of the if else structure
+        // if the parent is smaller, 
+        // swap until it's larger / not possible
 
         // get the index of the parent
         int index_parent = (index - 1) / 2;
@@ -91,6 +94,12 @@ void substitute_in_heap(vector <T> &v, int index, T& element){
         }
 
         //section: parent is larger-------------------------------
+        // second part of the if else structure
+        // if the parent is larger, while loop above didn't run 
+        // check the heap condition for the current item
+        // if the parent was smaller, while loop did run
+        // but the heap condition check will terminate almost 
+        // immediately 
 
         // ensure the heap condition
         heapify(v, index);
